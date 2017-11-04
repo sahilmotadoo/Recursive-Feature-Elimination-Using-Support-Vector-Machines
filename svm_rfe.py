2d8aad8e7bacc9214330626bcf90dc367b7f4b8b
Sahil Motadoo <sahil.motadoo9192@gmail.com>
2017-11-03 21:51:08 -0700

    Python program use for implementing RFE.

---
+++ svm_rfe.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import warnings
+
+#Supress Warnings
+warnings.filterwarnings("ignore")
+
+#Set parameter
+C = 1.0
+
+#Create DataFrame
+df = pd.read_csv('mbscores.csv')
+nFeatures = len(df.columns) - 1
+
+#Split data and scores from modified .CSV file
+samples = df.filter(['HMM', 'SSD', 'OGS'])
+scores = df.filter(['Scores'])
+
+#Recursively eliminate features based on the lowest weight
+rfeIndex = nFeatures
+while True:
+	#Split into training and testing
+	x_train, x_test, y_train, y_test = train_test_split(samples, scores, test_size = 0.50, train_size = 0.50)
+	
+	#Create SVM model using a linear kernel
+	model = svm.SVC(kernel='linear', C=C).fit(x_train, y_train)
+	coef = model.coef_
+
+	#Print co-efficients of features
+	for i in range(0, nFeatures):
+		print samples.columns[i],":", coef[0][i]
+	
+	#Find the minimum weight among features and eliminate the feature with the smallest weight
+	min = coef[0][0]
+	index = 0
+	for i in range(0, rfeIndex):
+		if min > coef[0][i]:
+			index = index + 1
+			min = coef[0][i]
+	if len(samples.columns) == 1:
+		print "After recursive elimination we have the", samples.columns[index], "feature with a score of:", min
+		break
+	else:
+		print "Lowest feature weight is for", samples.columns[index], "with a value of:", min
+		print "Dropping feature", samples.columns[index]  
+
+		#Drop the feature in the 'samples' dataframe based on the lowest feature index
+		samples.drop(samples.columns[index], axis = 1, inplace = True)
+		
+		rfeIndex = rfeIndex - 1
+		nFeatures = nFeatures - 1
